3c0caedee5a8a3ce5e4a89cc8e720beb45182492
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-08-27 00:30:39 +0500

    Add files via upload

---
+++ Chapter-five/script.js
@@ -0,0 +1,170 @@
+//Q1
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 + num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+
+//Q2
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 - num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 * num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 / num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 % num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+//Q3
+// (A).
+// var name
+
+// (B).
+// var price
+// document.write("Value after variable declaration is:" + price)
+
+// (C).
+// var price = 5
+// document.write(price)
+
+// (D).
+// var price = 5
+// document.write("Initialize the variable:" + price)
+
+// (E).
+// var price = 5
+// document.write(++price)
+
+// (F).
+// var price = 5
+// document.write("Value after increment is:" + ++price)
+
+// (G).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write(++price3)
+
+// (H).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write("Value after addition is:" + ++price3)
+
+// (I).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write(price3--)
+
+// // (J).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write("Value after decrement is:" + price3--)
+
+// (K).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 / price2
+// document.write(price3)
+
+// (L).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 / price2
+// document.write("The remainder is :" + price3)
+
+//Q4
+// var price1 = 600
+// var price2 = 5
+// var price3 = price1 * price2
+
+// document.write("Tota cost to buy 5 tickets to a movie is " + price3 + "PKR")
+
+//Q5
+// var num9 = 4
+
+// document.write(`${num9} * 1 = ${num9 * 1}`)
+// document.write(`${num9} * 2 = ${num9 * 2}`)
+// document.write(`${num9} * 3 = ${num9 * 3}`)
+// document.write(`${num9} * 4 = ${num9 * 4}`)
+// document.write(`${num9} * 5 = ${num9 * 5}`)
+// document.write(`${num9} * 6 = ${num9 * 6}`)
+// document.write(`${num9} * 7 = ${num9 * 7}`)
+// document.write(`${num9} * 8 = ${num9 * 8}`)
+// document.write(`${num9} * 9 = ${num9 * 9}`)
+// document.write(`${num9} * 10 = ${num9 * 10}`)
+
+//Q7
+// var cart1 = 650
+// var cart2 = 3
+// var cart3 = 100
+// var cart4 = 7
+// var cart5 = 100
+
+// document.write("Price of item 1 is" + cart1 )
+// document.write("Ordered quantity of item 1 is" + cart2)
+// document.write("Price of item 2 is" + cart3)
+// document.write("Ordered Quantity of item 2 is" + cart4)
+// document.write("Shipping charges" + cart5)
+
+//document.write("Total cost of your order is \n" + cart1 * cart2 + cart3 * cart4 + cart5)
+
+//Q8
+// var total = 980
+// //document.write("Total marks:" + total)
+// var obtained = 804
+// //document.write("Marks obtained:" + obtained)
+
+// document.write("Percentage" + obtained  / total * 100)
+
+//Q9
+// var us = (10 * 104.80) + (25 * 28)
+
+// document.write("Total Currency in PKR:\n" + us)
+
+//Q10
+// var add1 = 5
+// var add2 = 5
+// var mul = 10
+// var divi = 2
+
+// document.write (add1 + add2 * mul / divi)
+
+
+//Q11
+// var current = 2016
+// document.write ("Current year:" + current)
+// var birth = 1992
+// document.write ("Birth year:" + birth )
+
+// document.write ("Your age is:" + current - birth )
+
+//Q13
+//document.write ("Favourite snacke : chocolate chip")
+//var age = 15
+// document.write("Current age:" + age)
+//var max = 65
+//document.write("Estimated Maximum age:" + max)
+//var snackes = 3
+//document.write("Amount of snackes per day:" + snackes)
+
+//document.write("You will need" + max - age * snackes + "chocolate chip to last you until the rip old age of " + max)
+
---
+++ Chapter-four/script.js
@@ -0,0 +1,42 @@
+//Q1
+// var name, fullName, userName
+
+//Q2
+
+//Legal
+//1. name
+//2. FullName
+//3. userName
+//4. father_name
+//5. name123
+
+// Illegal
+//1. 12name
+//2. 12name123
+//3. -name
+//4. =fullname
+//5. (name)
+
+//Q3
+// (A).
+// var number1 = (",")
+// var number2 = ("\nnumbers")
+// var number3 = ("\n$\n")
+// var number4 = ("_")
+
+// document.write("Variable names can only contain\n" + number1 + number2 + number3 + "and\n" + number4 + "\nFor example : $my_1stVariable")
+
+// (B).
+// var letter1 = ("letter")
+// var letter2 = ("\n$\n")
+// var letter3 = ("_")
+
+// document.write("Variables must begin with a\n" + letter1 +  letter2 + "or" + letter3 + ". \n For example : $name, _name or name")
+
+//(C).
+// var sen= "\nsensitive"
+// document.write ("Variable names are case" + sen)
+
+//(D).
+var key= "\nkeywords"
+document.write ("Variable names should not be JS" + key)
