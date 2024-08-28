e0520da421a283b25b4f40994e628c04aab2df49
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-08-28 22:04:11 +0500

    Add files via upload

---
+++ Chapter-six/script.js
@@ -0,0 +1,77 @@
+//Q1
+// var result = 10
+// document.write("The value of a is: \n"+ result)
+
+// var result = 10
+// document.write("The value of ++a is:\n" + ++result + "\n Now the value of a is:" + result)
+
+// var result = 10
+// document.write("The value of ++a is:\n" + ++result + "\n Now the value of a is:" + ++result)
+
+// var result = 10
+// document.write("The value of ++a is:\n" + ++result + "\n Now the value of a is:" + result--)
+
+// var result = 10
+// document.write("The value of ++a is:\n" + ++result + "\n Now the value of a is:" + --result)
+
+//Q2
+// var a = 2
+// var b = 1
+//document.write(--a) 1
+// document.write(--a - --b) 1
+//document.write(--a - --b + ++b) 2
+//document.write(--a - --b + ++b + b--) 3
+
+// var result = (7)
+
+// document.write("a is:" + a + "b is:" + b + "Result is: " + result)
+
+// Q3
+
+// var an = prompt("Please enter your number")
+// alert("hello"+" "+an)
+
+// Q5
+
+// var an = prompt("Please enter your number");
+
+// if (an) {
+//   parseInt(an);
+// } else {
+//   an = 5;
+// }
+
+// document.write(an + " x 1 = " + an * 1 + "<br>");
+// document.write(an + " x 2 = " + an * 2 + "<br>");
+// document.write(an + " x 3 = " + an * 3 + "<br>");
+// document.write(an + " x 4 = " + an * 4 + "<br>");
+// document.write(an + " x 5 = " + an * 5 + "<br>");
+// document.write(an + " x 6 = " + an * 6 + "<br>");
+// document.write(an + " x 7 = " + an * 7 + "<br>");
+// document.write(an + " x 8 = " + an * 8 + "<br>");
+// document.write(an + " x 9 = " + an * 9 + "<br>");
+// document.write(an + " x 10 = " + an * 10 + "<br>");
+
+//Q6
+// var name = prompt("User Name");
+// console.log(name);
+
+
+
+// var sub_1 = prompt("Write your subject 1");
+// var sub_2 = prompt("Write your subject 2");
+// var sub_3 = prompt("Write your subject 3");
+
+// var total_mark = 100
+
+// var obt_1 = prompt("Write your marks obtained in subject 1");
+// var obt_2 = prompt("Write your marks obtained in subject 2");
+
+// document.write(`${sub_1} &nbsp ${total_mark} ${parseInt(obt_1)} ${parseInt(obt_1) / total_mark * 100} <br>`);
+// document.write(`${sub_2} ${total_mark} ${parseInt(obt_2)} ${parseInt(obt_2) / total_mark * 100} <br>`);
+// document.write(`${sub_3} ${total_mark} ${parseInt(obt_2)} ${parseInt(obt_2) / total_mark * 100} <br>`);
+
+// var total_obt = parseInt(obt_1) + parseInt(obt_2) * 2
+// var total_per = total_obt / 300 * 100
+
+// document.write(`&nbsp &nbsp ${total_mark * 3} ${parseInt(obt_2) * 2 + parseInt(obt_1)} ${total_per} <br>`);
