4245c3093e9c3e9ee8414adbab6c71574ea9988c
Mariyam Sana <mariyamsanaarain710@gmail.com>
2024-09-01 00:11:44 +0500

    Add files via upload

---
+++ Chapter-nine-eleven/script.js
@@ -0,0 +1,109 @@
+//Q1
+// var City = prompt("Enter City Name")
+// if (City == "karachi"){
+//     document.write ("Welcome to city of lights")
+// }else{
+
+// }
+
+//Q2
+// var male = prompt("Gender")
+// if (male == "Male"){
+//     document.write ("Good Morning sir")
+// }else if(male == "Female"){
+//     document.write ("Good Morning Ma'dam")
+// }else{
+
+// }
+
+//Q3
+// var color = prompt ("Road traffic signal")
+// if (color == "Red"){
+//     document.write("Must Stop")
+// }else if(color == "Yellow"){
+//     document.write("Ready to move")
+// }else if(color == "Green"){
+//     document.write("Move now")
+// }
+
+//Q4
+// var fuel = prompt("Fuel in car")
+// if (fuel < 0.25){
+//     document.write("please refill your fuel")
+// }
+
+//Q5
+// var a = 4;
+// if (++a){
+// alert("given condition for variable a is true");
+// }
+
+//b.
+// var b = 82;
+// if (b++){
+// alert("given condition for variable b is false");
+// }
+
+//c.
+// var c = 12;
+// if (c++){
+// alert("condition 1 is true");
+// }
+// if (c){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// //d.
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+//e.
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//f.
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+//Q6
+// var eng = prompt("English ")
+// var urdu = prompt("Urdu")
+// var Math = prompt("Math")
+// var sub = eng + urdu + Math
+
+//Q7
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+// if (userGuess === secretNumber) {
+//     alert("Bingo! Correct answer.");
+// } else if (userGuess + 1 === secretNumber) {
+//     alert("Close enough to the correct answer.");
+// } else {
+//     alert("Sorry, that's not correct. The secret number was " + secretNumber + ".");
+// }
+
+//Q8
+// var number = prompt("Enter a number to check if it is divisible");
+// var num = parseInt(3)
+
+// if (number % num === 0) {
+//     alert("The number " + number + " is divisible by 3.");
+// } else {
+// }
+
